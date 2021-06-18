51e4c260f1ec962c54f0292e97ec8adc0c30f395
Hanyu <rollr76518@gmail.com>
2021-06-18 23:13:13 +0800

    Crash at ATableViewCell.setup()

---
+++ CellPractice/BViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BViewController.swift
+//  CellPractice
+//
+//  Created by Ryan on 2021/6/18.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+
+    private lazy var tableView: UITableView = makeTableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate(
+            [
+                tableView.topAnchor.constraint(equalTo: view.topAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ]
+        )
+    }
+}
+
+extension BViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ATableViewCell", for: indexPath) as! ATableViewCell
+        cell.setup(text: "\(indexPath)")
+        return cell
+    }
+}
+
+extension BViewController {
+    func makeTableView() -> UITableView {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.register(ATableViewCell.self, forCellReuseIdentifier: "ATableViewCell")
+        return tableView
+    }
+}
--- CellPractice/ViewController.swift
+++ CellPractice/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ATableViewCell", for: indexPath) as! ATableViewCell
+        cell.setup(text: "\(indexPath)")
+        return cell
+    }
+}
+
+class ATableViewCell: UITableViewCell {
+
+    @IBOutlet weak var aLabel: UILabel!
+
+    func setup(text: String?) {
+        aLabel.text = text
+    }
+}
